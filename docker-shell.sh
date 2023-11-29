14bd57077039dc3ed3d7f6e0bede1e507fc51285
Patrik Stutz <patrik.stutz@gmail.com>
2023-11-29 11:00:22 +0100

    also mount git directory into container

--- docker-shell.sh
+++ docker-shell.sh
@@ -3,4 +3,5 @@
 docker run -it --rm \
    --workdir="/repo" \
    --mount type=bind,source=$(pwd),target=/repo\
+   --mount type=bind,source=$(pwd)/.git,target=/repo/.git\
    nixpkgs/nix-flakes bash
